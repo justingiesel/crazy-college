091fe9338668cecfc5a71a56be24ef7589a467f3
Justin <jonesy399@gmail.com>
2020-04-19 07:08:27 +0700

    Remove themes & create main files

---
+++ wp-content/themes/crazy-college/header.php
@@ -0,0 +1,10 @@
+<h1>Header</h1><?php
+
+    while(have_posts()){
+        the_post(); ?>
+        <h1>This is a page, NOT a post</h1>
+        <h2><?php the_title() ?></h2>
+        <?php the_content();?>
+    <?php }
+
+?>
